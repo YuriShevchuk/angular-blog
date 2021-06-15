43533cb8d75e4641412158810d8dd92c77773ddf
YuriShevchuk <yurii.shev4uk@gmail.com>
2021-06-15 09:12:20 +0300

    add guards

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { User } from 'src/app/shared/interfaces';
 import { AuthService } from '../shared/services/auth.service';
 
@@ -12,13 +12,21 @@ import { AuthService } from '../shared/services/auth.service';
 export class LoginPageComponent implements OnInit {
   public form: FormGroup;
   public submitted = false;
+  public message: string;
 
   constructor(
     public auth: AuthService,
-    private router: Router
+    private router: Router,
+    private route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((params: Params) => {
+      if (params['loginAgain']) {
+        this.message = 'Введіть свої дані'
+      }
+    })
+
     this.form = new FormGroup({
       email: new FormControl(null, [
         Validators.required, 
